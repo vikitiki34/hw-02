488b3197c388980b4c2ba82c665b5570d35b9009
vikitiki34 <146087262+vikitiki34@users.noreply.github.com>
2024-04-17 12:07:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // ждёт загрузки документа
+     var menu = document.getElementById('menu'); 
+    if(x.className === "topnav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className="topnav";
+    }
+}
